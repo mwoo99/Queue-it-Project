faa1b39f9e51b80771a0c22fd183e38d43c14548
mwoo99 <michaelwoo99@gmail.com>
2022-04-20 17:32:23 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //module requirements
 //npm i puppeteer puppeteer-extra puppeteer-extra-plugin-stealth  || stealth mode
 //npm i @extra/recaptcha   || captcha solver module
-//**NOT NEEDED** npm i puppeteer-extra-plugin-adblocker  || adblocker, to make faster?
 const https = require('https')
 const fs = require('fs');
 const puppeteer = require('puppeteer-extra');
@@ -20,62 +19,135 @@ puppeteer.use(
   )
 */
 
-let saleURL = 'https://google.com';
+//let saleURL = 'https://google.com';
 //let saleURL = 'https://httpbin.org/headers';
-//let saleURL = 'https://bot.sannysoft.com';
+let saleURL = 'https://bot.sannysoft.com';
 //let saleURL = 'https://patrickhlauke.github.io/recaptcha/';
 //let saleURL = 'https://direct.playstation.com/en-us/hardware/ps5';
 
-const PROXY_SERVER_IP = '88.216.39.167';
-const PROXY_SERVER_PORT = '5183';
-const PROXY_USERNAME = 'BCJTX2997';
-const PROXY_PASSWORD = 'ADENWY24';
+var proxies = fs.readFileSync('.\\qbypass_pupp\\proxies.txt').toString().split("\n");
+
+
  
-async function main() {
+async function main(proxy) {
+    const proxyarr = proxy.split(":");
+    console.log(proxyarr);
+    const PROXY_SERVER_IP = proxyarr[0];
+    const PROXY_SERVER_PORT = proxyarr[1];
+    const PROXY_USERNAME = proxyarr[2];
+    const PROXY_PASSWORD = proxyarr[3];
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         args: [
-            `--proxy-server=http://${PROXY_SERVER_IP}:${PROXY_SERVER_PORT}`,
-            "--window-size=300,500"
+        //   `--proxy-server=http://${PROXY_SERVER_IP}:${PROXY_SERVER_PORT}`,
+        //   "--window-size=300,500"
         ]
     });
-    const page = await browser.newPage();
+    
 
-    //auth proxy
-    await page.authenticate({
+    const initPage = (async (url)=>{
+        const context = await browser.createIncognitoBrowserContext();
+        const page = await context.newPage();
+        await page.setDefaultTimeout(0);
+        /*auth proxy
+        await page.authenticate({
         username: PROXY_USERNAME,
         password: PROXY_PASSWORD,
+        });*/
+
+        //ignores images, css, etc
+        await page.setRequestInterception(true);
+        page.on('request', (request) => {
+            if (['image', 'stylesheet', 'font', 'script'].indexOf(request.resourceType()) !== -1) {
+                request.abort();
+            } else {
+                request.continue();
+            }
+        });
+
+        //set event listener for page
+        page.on("domcontentloaded", async (response) =>{
+            //await page.solveRecaptchas();
+            //console.log('response: ', response.status()); handle bans/errors
+            const mycookies = await page.cookies();
+            console.log(mycookies);
+            mycookies.forEach(async thiscookie => {
+                if (thiscookie.name.includes("1P_JAR")) {     //change to includes 'Queue-it-token', when passed queue -> saves session and opens a visible browser with session info
+                    console.log("cookie: "+thiscookie.name);
+                    const ls = await page.evaluate(() => JSON.stringify(localStorage));
+                    const ss = await page.evaluate(() => JSON.stringify(sessionStorage));
+                    //console.log(ls);
+                    await initHeadfulBrowser(mycookies,ls,ss);
+                    await page.removeAllListeners();
+                    await browser.close();
+                    //if (browser && browser.process() != null) browser.process().kill('SIGINT');
+                }
+            }
+            ); 
+        })
+        await page.goto(url,{waitUntil: "networkidle0"});
+        //await page.waitForTimeout(2500);
+        //await page.goto("https://google.com");
     });
 
-    await page.setDefaultTimeout(0);
+    //reopen a visible browser and restore session
+    const initHeadfulBrowser = (async (cookies,ls,ss)=>{
+        //console.log(ls);
+        //console.log(ss);
+        const browser2 = await puppeteer.launch({
+            headless: false,
+            args: [
+        //   `--proxy-server=http://${PROXY_SERVER_IP}:${PROXY_SERVER_PORT}`,
+            "--window-size=500,500",
+            "--window-position=0,0",
+        ]
+        })
+        /*auth proxy
+        await page.authenticate({
+        username: PROXY_USERNAME,
+        password: PROXY_PASSWORD,
+        });*/
 
-    //await page.setRequestInterception(true);
-    //page.on("request", (request) => { request.continue()});
+        const headfulPage = await browser2.newPage();
+        await headfulPage.setDefaultTimeout(0);
+        
+        //restore cookies
+        await headfulPage.setCookie(...cookies);
+        await headfulPage.goto(saleURL);
 
-    //set event listener for page
-    page.on("response", async (response) =>{
-        if (response.status()==400) await browser.close();
-        //await page.solveRecaptchas();
-        //console.log('response: ', response.status()); handle bans/errors
-        const mycookies = await page.cookies();
-        await mycookies.forEach(thiscookie => {
-            if (thiscookie.name.includes("1P_JAR")) {     //change to includes 'Queue-it-token', when passed queue -> brings forward the browser
-                console.log("cookie: "+thiscookie.name);
-                page.bringToFront();
-                page.removeAllListeners();
+        //restore localstorage and sessionstorage
+        await headfulPage.evaluate(ls => {
+            for (const key in ls) {
+              localStorage.setItem(key, ls[key]);
             }
-        }
-        ); 
+        }, ls);
+        await headfulPage.evaluate(ss => {
+            for (const key in ss) {
+              sessionStorage.setItem(key, ss[key]);
+            }
+        }, ss);  
     })
-    await page.goto(saleURL,{waitUntil: "networkidle0"});
-};
+    
 
+    //opens M incognito pages with unique sessions,cookies,etc | all M pages will use same proxy
+    const openMpages = (async (M,url)=>{
+        for (var i=0; i<M ; i++){
+            try {
+                initPage(url);
+            } catch (error) {
+                console.log(error);
+            } 
+        }
+    })
+    openMpages(25,saleURL);    
+}
+
+//runs for N proxies
 function runNtimes(N){
     for (var i=0; i < N;i++){
-        main();
+        const proxystr = proxies[(Math.random() * proxies.length) | 0].slice(0,-1);
+        main(proxystr);
     }
 }
 
-runNtimes(2);
-
-//todo 1.read proxy.txt , 2.captcha solver, 3.bringToFront() as alert not ideal, 4.lessen pc resource strain(launch args,adblock,ignore useless stuff)
+runNtimes(1); //edit to number of proxies you want to run
